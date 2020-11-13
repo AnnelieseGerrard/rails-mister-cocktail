e8dff96ec3e2fd16397c1592d5b313a1dbf9697b
Anneliese Gerrard <anneliesegerrard@hotmail.com>
2020-11-13 16:50:56 +0000

    some styling complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+file = URI.open('https://images.unsplash.com/photo-1601924381523-019b78541b95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+mojito = Cocktail.new(name: "Mojito")
+mojito.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+mojito.save
+
+file = URI.open('https://images.unsplash.com/photo-1602061257507-e0150debda3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=939&q=80')
+peach_bellini = Cocktail.new(name: "Peach Bellini")
+peach_bellini.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+peach_bellini.save
+
+file = URI.open('https://images.unsplash.com/photo-1576670391698-81458215a5e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80')
+espresso_martini = Cocktail.new(name: "Espresso Martini")
+espresso_martini.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+espresso_martini.save
+
+file = URI.open('https://images.unsplash.com/photo-1601924381523-019b78541b95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+watermelon_martini = Cocktail.new(name: "Watermelon Martini")
+watermelon_martini.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+watermelon_martini.save
 
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Peach Belini")
-Cocktail.create(name: "Espresso Martini")
-Cocktail.create(name: "Watermelon Martini")
-Cocktail.create(name: "Pornstar Martini")
-Cocktail.create(name: "Rum Punch")
-Cocktail.create(name: "Caipirinha")
-Cocktail.create(name: "Cosmopolitan")
-Cocktail.create(name: "Old Fashioned")
+file = URI.open('https://images.unsplash.com/photo-1588134034989-cc5525cec241?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=954&q=80')
+strawberry_martini = Cocktail.new(name: "Strawberry Martini")
+strawberry_martini.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+strawberry_martini.save
+
+file = URI.open('https://images.unsplash.com/photo-1553451133-8083c47243d6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1034&q=80')
+rum_punch = Cocktail.new(name: "Rum Punch")
+rum_punch.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+rum_punch.save
+
+file = URI.open('https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80')
+caipirinha = Cocktail.new(name: "Caipirinha")
+caipirinha.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+caipirinha.save
+
+file = URI.open('https://images.unsplash.com/photo-1594026290778-b00634357f20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+cosmopolitan = Cocktail.new(name: "Cosmopolitan")
+cosmopolitan.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+cosmopolitan.save
+
+file = URI.open('https://images.unsplash.com/photo-1595977437232-9a0426ebfe4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')
+old_fashioned = Cocktail.new(name: "Old Fashioned")
+old_fashioned.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+old_fashioned.save
+
 
 
 Ingredient.create(name: "lemon")
